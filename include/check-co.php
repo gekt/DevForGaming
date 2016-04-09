9e5d3e97f5de3bde14f14523eda85a70f1f2126b
Julien <julien.thomas018@live.fr>
2016-04-09 12:58:17 +0200

    Message erreur connexion (FIX)

--- include/check-co.php
+++ include/check-co.php
@@ -17,7 +17,7 @@ if (isset($_POST['connexion'])){
     }
     $pass_hache_co = sha1($_POST['mdp_co']);
     if ($pass_hache_co != $mdp) {
-        $erreur = "<div><div class=\"row container center-align\"><div class=\"header\"><strong>Erreur!</strong> Les mots de passe ne corresponde pas!</div></div></div>";
+        $erreur = "<div><div class=\"row center-align\"><div class=\"header\"><strong>Erreur!</strong> Le mot de passe est incorrect !</div></div></div>";
     }else{
         session_start();
         $_SESSION['login'] = $_POST['pseudo'];
